15e2f5517536a34c3e330590075b5caf7726a966
Rostik Wozniak <rostik19wozniak@icloud.com>
2024-02-28 09:01:42 +0100

    hide on smal devices header

--- src/components/sections/HeroSection/SmallDevices/CardsSection.tsx
+++ src/components/sections/HeroSection/SmallDevices/CardsSection.tsx
@@ -5,8 +5,8 @@ import CardItem from "./CardItem";
 
 const CardsSection = () => {
   return (
-    <section className="space-y-20 bg-services-cards bg-cover pt-28 xl:pb-80">
-      <h2 className="text-center text-3xl font-bold text-black-75 sm:text-4xl">
+    <section className="space-y-20 bg-services-cards bg-cover lg:pt-28 xl:pb-80">
+      <h2 className="hidden text-center text-3xl font-bold text-black-75 sm:block sm:text-4xl">
         Our Services
       </h2>
       <MaxWidthWraper className="flex flex-col  px-0 sm:gap-10 sm:px-2.5 xl:flex-row xl:gap-0">
