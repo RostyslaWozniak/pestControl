49fccdf44c7c06b8972dde34b08b7249f06c3af6
Rostik Wozniak <rostik19wozniak@icloud.com>
2024-02-28 09:00:24 +0100

    delete lazy loading

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,7 +5,7 @@ const Map = () => {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d305.87054456359067!2d21.068577527903415!3d52.17134150246102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47192d5789daf843%3A0xcb286e01fd0b374d!2sDalmyt!5e0!3m2!1spl!2spl!4v1708798659690!5m2!1spl!2spl"
         width="100%"
         height="100%"
-        loading="lazy"
+        // loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
     </div>
